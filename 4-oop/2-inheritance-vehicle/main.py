655fcba709eb4708a821ea9bcd5be241537438e6
darkanemic <qmail4444@gmail.com>
2024-10-26 20:26:46 +0300

    init 5-homework-2-sync-calculation-with-timer

--- 4-oop/2-inheritance-vehicle/main.py
+++ 4-oop/2-inheritance-vehicle/main.py
@@ -5,23 +5,23 @@ class Vehicle:
         self.year = year
 
     def get_info(self):
-        return f'Марка: {self.make}, Модель: {self.model}, Год: {self.year}'
+        return f"Марка: {self.make}, Модель: {self.model}, Год: {self.year}"
 
 
 class Car(Vehicle):
     def start_engine(self):
-        print(f'Машина {self.model} завелась!')
+        print(f"Машина {self.model} завелась!")
 
 
 class Bicycle(Vehicle):
 
     def ring_bell(self):
-        print(f'Звенит звонок велосипеда {self.model}!')
+        print(f"Звенит звонок велосипеда {self.model}!")
 
 
-vehicle = Vehicle('Москвич', '412', '1977')
-cybertruck = Car('Tesla', 'Cybertruck', '2023')
-orlenok = Bicycle('Вайрас', 'Орлёнок', '1962')
+vehicle = Vehicle("Москвич", "412", "1977")
+cybertruck = Car("Tesla", "Cybertruck", "2023")
+orlenok = Bicycle("Вайрас", "Орлёнок", "1962")
 print(vehicle.get_info())
 print(cybertruck.get_info())
 cybertruck.start_engine()
